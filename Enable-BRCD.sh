efc08f9b6fe636574a1889524483ddca134a4d67
RileyMarquis3 <15660294+RileyMarquis3@users.noreply.github.com>
2024-08-25 03:53:30 -0500

    Create Enable-BRCD.sh

---
+++ Enable-BRCD.sh
@@ -0,0 +1,9 @@
+##### Enable bashrc.d #####
+# User specific aliases and functions
+if [ -d ~/.bashrc.d ]; then
+        for rc in ~/.bashrc.d/*; do
+                if [ -f "$rc" ]; then
+                        . "$rc"
+                fi
+        done
+fi
